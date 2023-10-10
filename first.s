f8fc71b34e7386cf6d66c0ccfedc450c19860474
marco-cuconato <99355087+marco-cuconato@users.noreply.github.com>
2023-10-10 12:45:59 -0400

    add first.s and makefile

---
+++ first.s
@@ -0,0 +1,8 @@
+/* -- first.s */
+ /* This is a comment */
+ .global main /* entry point must be global */
+ .func main /* ’main’ is a function */
+
+ main: /* This is main */
+ mov r0, #2 /* Put a 2 into register r0 */
+ bx lr /* Return from main */
