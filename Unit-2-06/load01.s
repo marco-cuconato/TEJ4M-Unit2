12f429dab32248a1f143e6026cada91a6ba82513
marco-cuconato <99355087+marco-cuconato@users.noreply.github.com>
2023-10-30 12:41:05 -0400

    Fixed coding error

--- Unit-2-06/load01.s
+++ Unit-2-06/load01.s
@@ -4,32 +4,32 @@
 .data
 
 /* Ensure variable is 4-byte aligned */
-.balign
+.balign 4
 /* Define storage for myvar1 */
 myvar1:
-       /* Contents fo myvar1 is 4 byte containing the value  3 */
-       .word3 
+	/* Contents of myvar1 is 4 bytes containing the value 3 */
+	.word 3
 
 /* Ensure variable is 4-byte aligned */
-.balign4 
-/* Define storage fro myvar1 */
-myvar2 :
-        /* contents fo myvar2 is 4 byte containing the value 4 */
-        .word4
+.balign 4
+/* Define storage for myvar1 */
+myvar2:
+	/* Contents of myvar2 is 4 bytes containing the value 4 */
+	.word 4
 
-/*  -- Code section */
+/* -- Code section */
 .text
 
 /* Ensure variable is 4-byte aligned */
-.balign4
+.balign 4
 .global main
-main: 
-     ldr r1, addr_of_myvar1 // r1 <- &myvar1
-	 ldr r1, [r1]           // r1 <- *r1
-	 ldr r2, addr_of_myvar2 // r2 <- &myvar2
-	 ldr r2, [r2]           // r2 <- *r2
-	 add r0, r1, r2         // r0 <- r1 + r2
-	 bx lr
+main:
+	ldr r1, addr_of_myvar1 // r1 <- &myvar1
+	ldr r1, [r1]           // r1 <- *r1
+	ldr r2, addr_of_myvar2 // r2 <- &myvar2
+	ldr r2, [r2]           // r2 <- *r2
+	add r0, r1, r2         // r0 <- r1 + r2
+	bx lr
 
 /*Labels needed to access data */
 addr_of_myvar1: .word myvar1
